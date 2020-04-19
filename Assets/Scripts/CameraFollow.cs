607167bc612c27f62abd9aac88f47676dba2bca4
Rezan Kizilgün <rezan@kizilguen.com>
2020-04-19 04:29:31 +0200

    Seperated player rotation from segway rotation & added terrain texture

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,12 @@ public class CameraFollow : MonoBehaviour
 {
     public float m_smoothTime = 1.0f;
     public float m_maxSpeed = 1.0f;
-    
+
+    public float m_cameraHeightOffset = 6.2f;
     public Vector2 m_centerOffset = new Vector2(0, -5.5f);
     public Vector2 m_minOffset = new Vector2(-1, -3);
     public Vector2 m_maxOffset = new Vector2(1, 3);
-
+    
     public Rigidbody m_playerRB;
     public PlayerMovement m_playerMovement;
     
@@ -27,7 +28,7 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         Vector3 targetPosition = m_playerRB.transform.position + new Vector3(m_centerOffset.x, 0, m_centerOffset.y);
-        targetPosition.y = transform.position.y;
+        targetPosition.y = m_playerRB.transform.position.y + m_cameraHeightOffset;
         {
             Vector3 playerVelocity = m_playerRB.velocity;
             float maxSpeedRatioX = Math.Abs(playerVelocity.x) / m_playerMovement.m_maxSpeed;
@@ -52,6 +53,7 @@ public class CameraFollow : MonoBehaviour
             }
         }
 
+        
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref m_cameraVelocity, m_smoothTime, m_maxSpeed,
             Time.deltaTime);
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,12 @@ public class PlayerMovement : MonoBehaviour
     public float m_maxSpeed = 100.0f;
     public float m_decelerationSpeed = 200.0f;
     public float m_maxRotationSpeed = 100.0f;
+    public float m_maxTiltAngle = 30.0f;
     public LayerMask m_mouseRayCastLayer;
-    
+
+    public Transform m_segwayBodySlotTransform;
+    public Transform m_playerBodySlotTransform;
+
     private Rigidbody m_rigidbody;
     private Camera m_camera;
 
@@ -48,6 +52,15 @@ public class PlayerMovement : MonoBehaviour
                 velocity.x = currentSpeed.x;
                 velocity.z = currentSpeed.y;
             }
+
+            // Segway Body Tilt
+            {
+                Quaternion segwayBodyRotation = m_segwayBodySlotTransform.localRotation;
+                Vector3 segwayBodyAngles = segwayBodyRotation.eulerAngles;
+                segwayBodyAngles.x = Mathf.Lerp(0, m_maxTiltAngle, currentSpeed.magnitude / m_maxSpeed);
+                segwayBodyRotation.eulerAngles = segwayBodyAngles;
+                m_segwayBodySlotTransform.localRotation = segwayBodyRotation;
+            }
         }
 
         if (Mathf.Abs(horizontalInput) < 0.05f)
@@ -62,14 +75,28 @@ public class PlayerMovement : MonoBehaviour
         m_rigidbody.velocity = velocity;
 
         {
+            Vector2 currentVelocity = new Vector2(velocity.x, velocity.z);
+            if (currentVelocity.magnitude > 0.1f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                    Quaternion.LookRotation(new Vector3(currentVelocity.x, 0, currentVelocity.y), Vector3.up), m_maxRotationSpeed * Time.deltaTime);
+            }
+        }
+
+        {
             Ray mouseRay = m_camera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(mouseRay, out RaycastHit hitInfo, 1000, m_mouseRayCastLayer.value))
             {
                 Vector3 targetPosition = hitInfo.point;
                 targetPosition.y = transform.position.y;
+
+                Quaternion tiltRotation = Quaternion.FromToRotation(Vector3.up, m_segwayBodySlotTransform.up);
                 
-                transform.rotation = Quaternion.RotateTowards(transform.rotation,
-                    Quaternion.LookRotation((targetPosition - transform.position).normalized, Vector2.up), m_maxRotationSpeed);
+                Quaternion targetRotation =
+                    Quaternion.LookRotation((targetPosition - transform.position).normalized, Vector3.up);
+
+                m_playerBodySlotTransform.rotation = Quaternion.RotateTowards(m_playerBodySlotTransform.rotation,
+                    tiltRotation * targetRotation, m_maxRotationSpeed * Time.deltaTime);
             }
         }
     }
